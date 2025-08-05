efd021728410f200514aff4007d9bac001a35593
groster <51245688+grosterr@users.noreply.github.com>
2025-08-05 16:03:47 +0000

    mario bricks

--- week_1/problem_set_1/mario-less/mario.c
+++ week_1/problem_set_1/mario-less/mario.c
@@ -1,7 +1,35 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void print_row(int spaces, int bricks);
+
 int main(void)
 {
-    
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        int bricks = i + 1;
+        int spaces = n - 1 - i;
+        print_row(spaces, bricks);
+    }
+}
+
+void print_row(int spaces, int bricks)
+{
+    for (int i = 0; i < spaces; i++)
+    {
+        printf(".");
+    }
+    for(int i = 0; i < bricks; i++)
+    {
+        printf("#");
+    }
+
+    printf("\n");
 }
