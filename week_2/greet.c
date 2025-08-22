4da53535e191efdf72619306e625638969c5d8dd
groster <51245688+grosterr@users.noreply.github.com>
2025-08-22 11:01:00 +0000

    Fri, Aug 22, 2025, 2:01 PM +03:00

--- week_2/greet.c
+++ week_2/greet.c
@@ -1,8 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
+int main(int argc, string argv[])
 {
-    string answer = get_string("What`s your name? ");
-    printf ("Hello %s\n", answer);
+    // string answer = get_string("What`s your name? ");
+    // printf ("Hello %s\n", answer);
+    printf("hello, %s\n", argv[0]);
 }
