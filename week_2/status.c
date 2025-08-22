c83beede4d8446949939f681aac025f531eed731
groster <51245688+grosterr@users.noreply.github.com>
2025-08-22 11:13:03 +0000

    Fri, Aug 22, 2025, 2:13 PM +03:00

---
+++ week_2/status.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argcm string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Missing command-line argument\n");
+        return 1
+    }
+    if
+
+}
