237f210f56b74dbf4fce520f05c325d8a9906276
groster <51245688+grosterr@users.noreply.github.com>
2025-08-22 10:46:24 +0000

    Fri, Aug 22, 2025, 1:46 PM +03:00

--- week_2/uppercase.c
+++ week_2/uppercase.c
@@ -9,17 +9,7 @@ int main(void)
     printf("After: ");
     for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if(s[i] >= 'a' && s[i] <= 'z')
-        {
-
-            printf("%c", toupper(s[i]));
-        }
-        else
-        {
-
-            printf("%c", s[i]);
-        }
-
+        printf("%c", toupper(s[i]));
     }
     printf("\n");
 }
