edbfd3ebd280b32e5ca80c1fda7e8e41a639bb7d
groster <51245688+grosterr@users.noreply.github.com>
2025-08-22 08:09:39 +0000

    Fri, Aug 22, 2025, 11:09 AM +03:00

--- week_2/hi.c
+++ week_2/hi.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int main(void)
-{
-    char c1 = 'h';
-    char c2 = 'i';
-    char c3 = '!';
+// {
+  //  char c1 = 'h';
+  //  char c2 = 'i';
+  //  char c3 = '!';
 
-    printf("%c %c %c\n", c1, c2, c3);
+    // printf("%c %c %c\n", c1, c2, c3);
+// }
+{
+    string t = "H i !";
+    string b = "B Y E !";
 }
